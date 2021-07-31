5830fce9b276be634b165bd91bb3bfb80d12fb3a
henriquehbr <henriqueborgeshbr@gmail.com>
2021-07-31 17:00:36 -0300

    feat: implement initial version of `nvim-startup`

---
+++ lua/nvim-startup/init.lua
@@ -0,0 +1,20 @@
+local startup_time_file_path = '/tmp/nvim-startuptime'
+
+function display_startup_time()
+    local startup_time_file = io.open(startup_time_file_path)
+
+    -- get the latest line from vim startup log
+    -- Lua has it's own pattern matching syntax, mainly because
+    -- a regex library would already be larger than lua itself
+    local startup_time = startup_time_file
+        :read('*a')
+        :match('([%d.]+)  [%d.]+: [-]+ NVIM STARTED [-]+')
+
+    startup_time_file:close()
+
+    print('Launched in ' .. startup_time .. ' ms')
+end
+
+return {
+    display_startup_time = display_startup_time
+}
