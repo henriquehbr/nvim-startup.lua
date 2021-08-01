195219880025a616f5e1a5db7396b8834cb13b6b
henriquehbr <henriqueborgeshbr@gmail.com>
2021-08-01 15:27:33 -0300

    refactor: chain startup time log reading and parsing methods

--- lua/nvim-startup/init.lua
+++ lua/nvim-startup/init.lua
@@ -8,16 +8,12 @@ local function display_startup_time()
     -- closes file handle and returns "nil" in case the file doesn't exists
     if not file_exists(vim.g.nvim_startup_file) then return nil end
 
-    local startup_time_file = io.open(g.nvim_startup_file)
-
     -- gets the latest line from vim startup log
     -- Lua has it's own pattern matching syntax, mainly because
     -- a regex library would already be larger than lua itself
-    local startup_time = startup_time_file
+    local startup_time = io.open(g.nvim_startup_file)
         :read('*a')
         :match('([%d.]+)  [%d.]+: [-]+ NVIM STARTED [-]+')
-
-    startup_time_file:close()
 
     -- clear startup time log
     -- removing it causes some cache related issues (hyphothesis)
