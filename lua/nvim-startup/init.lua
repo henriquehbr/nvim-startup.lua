d5fa438479a44559b6a81a44e6fb684561e8fcdc
henriquehbr <henriqueborgeshbr@gmail.com>
2021-08-01 16:10:57 -0300

    feat!: remove `display_startup_time()` wrapper function
    From now on, for initializing `nvim-startup`, a simple `require` will be
    enough, no need for calling methods

--- lua/nvim-startup/init.lua
+++ lua/nvim-startup/init.lua
@@ -1,23 +1,18 @@
 local file_exists = require 'nvim-startup.file_exists'
 
--- global variables
 local g = vim.g
+
+-- global variables
 g.nvim_startup_file = g.nvim_startup_file or '/tmp/nvim-startuptime'
 
-local function display_startup_time()
-    if not file_exists(g.nvim_startup_file) then return nil end
+if not file_exists(g.nvim_startup_file) then return nil end
 
-    local startup_time = io.open(g.nvim_startup_file)
-        :read('*a')
-        :match('([%d.]+)  [%d.]+: [-]+ NVIM STARTED [-]+')
+local startup_time = io.open(g.nvim_startup_file)
+    :read('*a')
+    :match('([%d.]+)  [%d.]+: [-]+ NVIM STARTED [-]+')
 
-    -- clear startup time log
-    -- removing it causes some cache related issues (hyphothesis)
-    io.open(g.nvim_startup_file, 'w'):close()
+-- clear startup time log
+-- removing it causes some cache related issues (hyphothesis)
+io.open(g.nvim_startup_file, 'w'):close()
 
-    print('Launched in ' .. startup_time .. ' ms')
-end
-
-return {
-    display_startup_time = display_startup_time
-}
+print('Launched in ' .. startup_time .. ' ms')
