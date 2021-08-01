40be4d879107dcc1dea68062d18b4bed62d3be09
henriquehbr <henriqueborgeshbr@gmail.com>
2021-08-01 16:16:06 -0300

    refactor: assign startup time pattern to a variable

--- lua/nvim-startup/init.lua
+++ lua/nvim-startup/init.lua
@@ -7,9 +7,8 @@ g.nvim_startup_file = g.nvim_startup_file or '/tmp/nvim-startuptime'
 
 if not file_exists(g.nvim_startup_file) then return nil end
 
-local startup_time = io.open(g.nvim_startup_file)
-    :read('*a')
-    :match('([%d.]+)  [%d.]+: [-]+ NVIM STARTED [-]+')
+local startup_time_pattern = '([%d.]+)  [%d.]+: [-]+ NVIM STARTED [-]+'
+local startup_time = io.open(g.nvim_startup_file):read('*a'):match(startup_time_pattern)
 
 -- clear startup time log
 -- removing it causes some cache related issues (hyphothesis)
