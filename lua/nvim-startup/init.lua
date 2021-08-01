0af495e8248771adbe17eb6742343d9aaa2d2aad
henriquehbr <henriqueborgeshbr@gmail.com>
2021-08-01 15:22:45 -0300

    refactor: check if startup time log exists before reading

--- lua/nvim-startup/init.lua
+++ lua/nvim-startup/init.lua
@@ -5,10 +5,10 @@ local g = vim.g
 g.nvim_startup_file = g.nvim_startup_file or '/tmp/nvim-startuptime'
 
 local function display_startup_time()
-    local startup_time_file = io.open(g.nvim_startup_file)
-
     -- closes file handle and returns "nil" in case the file doesn't exists
     if not file_exists(vim.g.nvim_startup_file) then return nil end
+
+    local startup_time_file = io.open(g.nvim_startup_file)
 
     -- gets the latest line from vim startup log
     -- Lua has it's own pattern matching syntax, mainly because
