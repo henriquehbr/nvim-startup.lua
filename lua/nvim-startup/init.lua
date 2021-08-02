b95cff5e8eb11f5fe6dc9e73daffa1f814bdec46
henriquehbr <henriqueborgeshbr@gmail.com>
2021-08-02 10:17:41 -0300

    fix: only run if startup time log exists

--- lua/nvim-startup/init.lua
+++ lua/nvim-startup/init.lua
@@ -1,17 +1,19 @@
-local file_exists = require 'nvim-startup.file_exists'
-
 local g = vim.g
 
 -- global variables
 g.nvim_startup_file = g.nvim_startup_file or '/tmp/nvim-startuptime'
 
-if not file_exists(g.nvim_startup_file) then return nil end
+local startup_time_file = io.open(g.nvim_startup_file):read('*all')
 
-local startup_time_pattern = '([%d.]+)  [%d.]+: [-]+ NVIM STARTED [-]+'
-local startup_time = io.open(g.nvim_startup_file):read('*a'):match(startup_time_pattern)
+if string.len(startup_time_file) > 0 then
+    local startup_time_pattern = '([%d.]+)  [%d.]+: [-]+ NVIM STARTED [-]+'
+    local startup_time = startup_time_file:match(startup_time_pattern)
 
--- clear startup time log
--- removing it causes some cache related issues (hyphothesis)
-io.open(g.nvim_startup_file, 'w'):close()
+    -- clear startup time log
+    -- removing it causes some cache related issues (hyphothesis)
+    io.open(g.nvim_startup_file, 'w'):close()
 
-print('Launched in ' .. startup_time .. ' ms')
+    print('Launched in ' .. startup_time .. ' ms')
+else
+    print('nvim-startup: running on the next (n)vim instance')
+end
