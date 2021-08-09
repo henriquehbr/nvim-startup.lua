228628a54779aa073d6e67a3347b22757e8a0938
henriquehbr <henriqueborgeshbr@gmail.com>
2021-08-09 10:51:32 -0300

    fix: validate startup time before passing it to `message`

--- lua/nvim-startup/init.lua
+++ lua/nvim-startup/init.lua
@@ -17,13 +17,12 @@ M.setup = function(options)
         and tonumber(startup_time_file:match(startup_time_pattern))
         or nil
 
-    -- replace the message `{}` placeholder with the startup time
-    local template_message = type(opts.message) == 'function'
-        and opts.message(startup_time)
-        or opts.message
-    local message = template_message:gsub('{}', startup_time .. ' ms')
-
     if startup_time_file and startup_time then
+        -- replace the message `{}` placeholder with the startup time
+        local template_message = type(opts.message) == 'function'
+            and opts.message(startup_time)
+            or opts.message
+        local message = template_message:gsub('{}', startup_time .. ' ms')
         print(message)
     elseif startup_time_file and not startup_time then
         print('nvim-startup: running on the next (n)vim instance')
